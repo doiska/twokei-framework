d557afeaa7935292466b57ee28c7c1b2fcf55855
doiska <doiskka@gmail.com>
2022-03-01 12:57:00 -0300

    ver update

--- src/utils/loader.ts
+++ src/utils/loader.ts
@@ -3,7 +3,7 @@ import fs from 'fs'
 const getAllFiles = (path: string, extension?: string) => {
     const dir = fs.readdirSync(path);
 
-    return dir.map((file) => {
+    return dir.map((file: string) => {
         return [`${path}/${file}`, file];
     })
 }
